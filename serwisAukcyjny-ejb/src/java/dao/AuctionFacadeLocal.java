1b25cf70e926d9de5694c99ecf89781c67720165
Marcin Milewski <marcin.milewski@mail.com>
2015-05-19 02:32:57 +0200

    first commit

---
+++ serwisAukcyjny-ejb/src/java/dao/AuctionFacadeLocal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.util.List;
+import javax.ejb.Local;
+import model.Auction;
+
+/**
+ *
+ * @author zaba37
+ */
+@Local
+public interface AuctionFacadeLocal {
+
+    void create(Auction auction);
+
+    void edit(Auction auction);
+
+    void remove(Auction auction);
+
+    Auction find(Object id);
+
+    List<Auction> findAll();
+
+    List<Auction> findRange(int[] range);
+
+    int count();
+    
+}
