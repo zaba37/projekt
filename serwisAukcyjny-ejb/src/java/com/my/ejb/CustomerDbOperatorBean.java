55bb004364f442676b78ce66039f5a55de52c063
Marcin Milewski <marcin.milewski@mail.com>
2015-05-19 16:40:39 +0200

    add customerDbOperatorBean and AuctionDbOperatorBean

---
+++ serwisAukcyjny-ejb/src/java/com/my/ejb/CustomerDbOperatorBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.my.ejb;
+
+import javax.ejb.Singleton;
+
+/**
+ *
+ * @author marcin
+ */
+@Singleton
+public class CustomerDbOperatorBean implements CustomerDbOperatorBeanRemote {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
